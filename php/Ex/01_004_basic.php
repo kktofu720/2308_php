082aa39511cda5880e81ea5ccff854d0b99499d0
kktofu720 <rlaalswn0209@naver.com>
2023-09-08 17:37:13 +0900

    0908_php_이론

---
+++ php/Ex/01_004_basic.php
@@ -0,0 +1,19 @@
+<?php
+// 화면에 출력 하는 방법 : print문,  echo문 등
+// 단순 출력의 경우 현업에서는 echo를 많이 사용
+// 차이점 : print문은 리턴값이 있고, echo는 없음
+// echo '안녕 PHP';
+// print '안녕 print';
+
+// 변수
+
+$num = 1;
+$str = '안녕, 변수';
+// $str = '바꿈';
+
+$sum = $num +5;
+$sum = $sum + $num;
+echo $sum;
+
+
+?>
