73507c56f4b4b81f00ea469dca2f2b3106c58c18
kktofu720 <rlaalswn0209@naver.com>
2023-10-05 00:31:44 +0900

    1005 miniboard css

--- side_project/mini_board/src/header.php
+++ side_project/mini_board/src/header.php
@@ -1,3 +1,42 @@
 <header>
 	<h1><a href="/mini_board/src/list.php">mini Board🐈</a></h1>
 </header>
+
+<div class="snowflakes" aria-hidden="true">
+  <div class="snowflake">
+  💚
+  </div>
+  <div class="snowflake">
+  🧡
+  </div>
+  <div class="snowflake">
+  💚
+  </div>
+  <div class="snowflake">
+  🧡
+  </div>
+  <div class="snowflake">
+  💚
+  </div>
+  <div class="snowflake">
+  🧡
+  </div>
+  <div class="snowflake">
+    💚
+  </div>
+  <div class="snowflake">
+    🧡
+  </div>
+  <div class="snowflake">
+    💚
+  </div>
+  <div class="snowflake">
+    🧡
+  </div>
+  <div class="snowflake">
+    💚
+  </div>
+  <div class="snowflake">
+    🧡
+  </div>
+</div>
