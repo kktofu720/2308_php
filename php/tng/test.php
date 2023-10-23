2180352eb9fb4094dd6bcf3a2d09ff2cb8684423
kktofu720 <rlaalswn0209@naver.com>
2023-10-23 14:28:33 +0900

    1023 구구단

--- php/tng/test.php
+++ php/tng/test.php
@@ -19,12 +19,12 @@
 // 	}
 // }
 
-class Test{
-	public static $tt = "a";
-}
+// class Test{
+// 	public static $tt = "a";
+// }
 
-Test::$tt = "b";
-echo Test::$tt;
+// Test::$tt = "b";
+// echo Test::$tt;
 
 
 // $obj = new Test();
@@ -33,8 +33,19 @@ echo Test::$tt;
 // echo "2 : ".$obj
 
 
+// 19단
 
+$file = fopen("19.txt", "a");
 
-
+for($i=1; $i <= 19; $i++) {
+	// echo "{$i}단\n";
+	fputs($file, "{$i}단\n");
+	for($j=1; $j <= 19; $j++) {
+		$mul = $i * $j;
+		// echo "{$i} X {$j} = {$mul}\n";
+		fputs($file, "{$i} X {$j} = {$mul}\n");
+	}
+}
+fclose($file);
 
 ?>
